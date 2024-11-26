8059f561898b2d07bfe6b19e34239f36927bca76
Mitul Patel <me@mitulpa.tel>
2024-11-26 13:59:30 -0500

    feat(class): human player class

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -87,3 +87,21 @@ def play(game, x_player, o_player, print_game=True):
 
     if print_game:
         print("It's a tie!")
+
+class HumanPlayer:
+    def __init__(self, letter):
+        self.letter = letter
+    
+    def get_move(self, game):
+        valid_square = False
+        val = None
+        while not valid_square:
+            square = input(f'{self.letter}, input your move (0 - 8): ')
+            try:
+                val = int(square)
+                if val not in game.available_moves():
+                    raise ValueError
+                valid_square = True
+            except ValueError:
+                print('Invalid move. Go Again')
+        return val
